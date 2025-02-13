707f7fb815c0507391b03cd15585d0822490ef74
Boboev Zohir <zohirbabaev151@gmail.com>
2025-02-13 10:49:24 +0500

    JS 2-dars
    Zeromax It-center 40 guruh

---
+++ js/main.js
@@ -0,0 +1,110 @@
+//if        => agar
+//else      => u holda
+//else if   => yokibo'lmasa
+
+//if("salom" == "salom"){
+//   console.log("Ulat teng");
+// }
+//else{
+//   console.log("Ulat teng emas")
+// }
+
+//if(45 >= 45){
+//    console.log("berilgan qiymatlar teng yoki katta");
+// }
+//else{
+//    console.log("Berilgan qiymatlar teng emas yoki birinchisi katta emas");
+// }
+
+//if(45 <= 45){
+//    console.log("Solishtiroyatgan son 45ga teng yoki kichik")
+// }
+//else{
+   //console.log("Solishtiloyatgan son 45ga teng yoki katta")
+// }
+
+//if("ya lublu suck dick" == "ya lublu suck dick"){
+//   console.log("Pust pri oshibki fera budet sosat dick")
+// }
+//else{
+//   console.log("Pri oshibki ya visvechivayus i fera sosat dick")
+// }
+
+//if("piski vokrug napadayut na mina" == "piski vokrug napadayut na mina"){
+//    console.log("Glotat dick's eta luchshoe")
+// }
+//else{
+//    console.log("Net sosat dick eta ni kruto")
+// }
+
+//if("52*25-122 <= 52/12+555"){
+//   console.log("Pisat dva zdes")
+// }
+//else{
+//    console.log("O net monster fimoz ego ukral")
+//}
+
+//if("76+25/12 >= 76*69-567"){
+    //console.log("Psat dva zdes")
+// }
+//else{
+//    console.log("O net monster fimoz ego ukral")
+// }
+
+//if(45 == 45){
+//    console.log("1-shart bajarildi");
+// }
+// else if(45 == 45){
+//    console.log("2-shart bajarildi");
+// }
+// else{
+//    console.log("Yuqoridagi shrtlardan birortasi bajarilmadi");
+// }
+
+//if(25 == 25){
+//    console.log("ertalab ovqat yiman")
+// }
+//else if(25 == 25){
+//    console.log("Obetda ovqat yevolaman")
+// }
+//else{
+//    console.log("Kechqurun babiram yevolaman")
+// }
+
+//if(56 == 56){
+//    console.log("Uyda uhlayman")
+// }
+//else if(56 == 56){
+//    console.log("Maktabda uhlayman")
+// }
+//else{
+//    console.log("Ishta uhlavolaman")
+//}
+
+//let a = 45;
+
+//if(a >= 45){
+//    console.log("a berilgan sondan kichik yoki teng")
+// }
+//else{
+//    console.log("a ning qiymati topilmadi")
+// }
+
+let a = 46;
+let b = 10;
+let c = 19;
+let d = 49;
+
+//if(a == 49 && b == 15 && c == 11 && d == 41){
+//    console.log("Shartlarnkng hammasi togri")
+// }
+//else{
+//    console.log("Bittasi tog'ri bomasa")
+// }
+
+if(a == 45 || b == 11 || c == 18 || d == 48){
+    console.log("Shartlarning kamidan bittasi togri")
+}
+else{
+    console.log("Shartlarning hammasi notogri")
+}
